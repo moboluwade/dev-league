1401aedd789bb7096b59bfe01c8ac570f3904681
moboluwade <folarinboluwade.com>
2023-11-22 23:58:33 +0100

    base init

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <App />
+    )
+  }
+])
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router}/>
+)
