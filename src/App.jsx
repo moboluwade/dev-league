a204e0063516a71e98af9725279d01b899f1027d
moboluwade <folarinboluwade.com>
2023-11-25 01:04:43 +0100

    resolve header conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import { Header } from './components/Header'
-import { Sponsor } from './components/Sponsor'
-import { Hero } from './views/Hero'
+
 
 const App = () => {
   return (
@@ -11,11 +10,6 @@ const App = () => {
         <Header />
         <Outlet />
       </div>
-
-      {/* can always copy an paste these tailwind padding classes to other components for uniformity*/}
-      {/*  lg:px-[7.5rem] md:px-12 px-4  */}
-      <Hero />
-      <Sponsor />
     </>
   )
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,15 @@
+import { Sponsor } from './components/Sponsor'
+import { Hero } from './views/Hero'
+
 const Home = () => {
-  return <div>Home</div>
+  return (
+    <>
+      {/* can always copy an paste these tailwind padding classes to other components for uniformity*/}
+      {/*  lg:px-[7.5rem] md:px-12 px-4  */}
+      <Hero />
+      <Sponsor />
+    </>
+  )
 }
 
 export default Home
