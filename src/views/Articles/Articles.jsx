0b75696ffcad52fc7f5f4a46cd92d4e14dd6abb1
moboluwade <folarinboluwade.com>
2023-12-01 03:30:28 +0100

    make impact section responsive on larger screens

--- src/views/Articles/Articles.jsx
+++ src/views/Articles/Articles.jsx
@@ -4,24 +4,25 @@ import Cards from './data'
 const Articles = () => {
   // const [readMore, setReadMore] = useState(false)
   return (
-    <div className="flex flex-col justify-center items-center gap-6 w-full mx-auto mt-8 text-center">
-      <div>
-        <h1 className="font-bold text-3xl pb-2">Our Articles</h1>
+    <div className="flex flex-col items-center justify-center w-full gap-6 mx-auto mt-8 text-center">
+      <div className='lg:px-[7.5rem] md:px-12 px-4 '>
+           <div className=''>
+        <h1 className="pb-2 text-3xl font-bold">Our Articles</h1>
         <span className=" text-normal">
           The latest industry news, interviews, technologies, and resources.
         </span>
       </div>
 
-      <div className="flex flex-wrap justify-center items-center gap-8">
+      <div className="flex flex-wrap items-center justify-center gap-8">
         {Cards.map((card) => {
           const { id, desc, img, person } = card
           return (
-            <div key={id} className="flex flex-col text-start gap-4">
+            <div key={id} className="flex flex-col gap-4 text-start">
               <div>
                 <img src={img} alt="image" />
               </div>
               <div className="innerWidth">
-                <h1 className="pt-4 pl-6 pb-2 textBig">
+                <h1 className="pt-4 pb-2 pl-6 textBig">
                   {/* {readMore ? title : title.substring(0, 50)} : title} */}
                 </h1>
                 <p className="pl-6 textLight">
@@ -30,7 +31,7 @@ const Articles = () => {
                 </p>
               </div>
 
-              <div className="flex gap-4 items-center pl-6">
+              <div className="flex items-center gap-4 pl-6">
                 <img src={person.img} alt="" />
                 <span className="text">{person.name}</span>
                 <span className="textLight">{person.date}</span>
@@ -39,6 +40,8 @@ const Articles = () => {
           )
         })}
       </div>
+      </div>
+   
     </div>
   )
 }
