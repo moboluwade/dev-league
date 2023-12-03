75f6321212774e42e08deda43d619e3c1d87d55a
moboluwade <folarinboluwade.com>
2023-12-03 15:31:13 +0100

    make articles section responsive

--- src/views/Articles/Articles.jsx
+++ src/views/Articles/Articles.jsx
@@ -3,43 +3,59 @@ import Cards from './data'
 
 const Articles = () => {
   // const [readMore, setReadMore] = useState(false)
+  const SliceText = (text) => {
+    const length = text.length;
+    if (length > 65) {
+      const slicedText = length.slice(0, 65)
+      const newText = slicedText + "..."
+      return newText
+    } else {
+      return text
+    }
+  }
   return (
-    <div className="flex flex-col items-center w-full text-center text-white bg-black h-fit">
-      <div className='lg:px-[7.5rem] md:px-12 px-4 max-w-[87.5rem]'>
-        <div className='flex flex-col pt-12'>
+    <div className="flex flex-col items-center w-full pb-12 text-center text-black h-fit">
+      <div className='lg:px-[7.5rem] md:px-12 px-4 max-w-[87.5rem] flex flex-col justify-center items-center'>
+
+        <div className='flex flex-col pt-6 pb-9'>
           <span className="pb-2 text-4xl font-semibold ">Our Articles</span>
           <span className="text-lg font-normal ">
             The latest industry news, interviews, technologies, and resources.
           </span>
         </div>
 
-        <div className="flex flex-wrap items-center justify-center gap-8">
+        <div className="flex flex-wrap items-start justify-center gap-8 pb-12 text-black">
           {Cards.map((card) => {
             const { id, title, desc, img, person } = card
             return (
-              <div key={id} className="flex flex-col max-w-[30rem] bg-white rounded-lg text-start">
+              <div key={id} className="flex flex-col border border-[#D4CECB] lg:max-w-[19rem] max-w-[18rem] bg-white rounded-lg text-start">
                 <div>
-                  <img className='rounded-t-lg w-fit' src={img} alt="image" />
+                  <img className='w-full rounded-t-lg' src={img} alt="image" />
                 </div>
-                <div className="">
-                  <h1 className="text-black  textBig">
+                <div className="px-3 pt-5 pb-3">
+                  <span className="text-xl font-semibold text-black ">
                     {title}
-                  </h1>
-                  <p className="w-full pl-6 textLight">
+                  </span>
+                  <p className="w-full pt-2 textLight">
                     {desc}
                     {/* <button>{readMore ? 'show less' : 'read more'}</button> */}
                   </p>
+
+                  <div className="flex items-center w-full gap-4 pt-7">
+                    <img src={person.img} alt="" />
+                    <span className="text">{person.name}</span>
+                    <span className="textLight">{person.date}</span>
+                  </div>
                 </div>
 
-                <div className="flex items-center w-full gap-4 pl-6">
-                  <img src={person.img} alt="" />
-                  <span className="text">{person.name}</span>
-                  <span className="textLight">{person.date}</span>
-                </div>
               </div>
             )
           })}
         </div>
+
+        <button className='h-12 px-5 py-3 bg-black rounded-lg w-fit text-[#FFF6F3]'>
+          Read Blog
+        </button>
       </div>
     </div>
   )
