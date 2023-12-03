ab474d0cca221661a8497675ff7123eb3e779151
moboluwade <folarinboluwade.com>
2023-12-03 15:49:44 +0100

    make article components responsive on large screens

--- src/views/Articles/Articles.jsx
+++ src/views/Articles/Articles.jsx
@@ -6,16 +6,16 @@ const Articles = () => {
   const SliceText = (text) => {
     const length = text.length;
     if (length > 65) {
-      const slicedText = length.slice(0, 65)
+      const slicedText = text.slice(0, 65)
       const newText = slicedText + "..."
       return newText
     } else {
-      return text
+      return text+"..."
     }
   }
   return (
     <div className="flex flex-col items-center w-full pb-12 text-center text-black h-fit">
-      <div className='lg:px-[7.5rem] md:px-12 px-4 max-w-[87.5rem] flex flex-col justify-center items-center'>
+      <div className='lg:px-[7.5rem] lg:w-[87.5rem] md:px-12 px-4 max-w-[87.5rem] flex flex-col justify-center items-center'>
 
         <div className='flex flex-col pt-6 pb-9'>
           <span className="pb-2 text-4xl font-semibold ">Our Articles</span>
@@ -24,21 +24,20 @@ const Articles = () => {
           </span>
         </div>
 
-        <div className="flex flex-wrap items-start justify-center gap-8 pb-12 text-black">
+        <div className="flex flex-row flex-wrap items-start justify-between w-full gap-8 pb-12 text-black">
           {Cards.map((card) => {
             const { id, title, desc, img, person } = card
             return (
-              <div key={id} className="flex flex-col border border-[#D4CECB] lg:max-w-[19rem] max-w-[18rem] bg-white rounded-lg text-start">
+              <div key={id} className="flex flex-col border border-[#D4CECB] lg:max-w-[22rem] max-w-[18rem] bg-white rounded-lg text-start">
                 <div>
                   <img className='w-full rounded-t-lg' src={img} alt="image" />
                 </div>
                 <div className="px-3 pt-5 pb-3">
                   <span className="text-xl font-semibold text-black ">
-                    {title}
+                    {title} <a className="" href="">Read more</a>
                   </span>
                   <p className="w-full pt-2 textLight">
-                    {desc}
-                    {/* <button>{readMore ? 'show less' : 'read more'}</button> */}
+                    {SliceText(desc)}
                   </p>
 
                   <div className="flex items-center w-full gap-4 pt-7">
