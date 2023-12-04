d58ae1de41830bc62c39cdba35f2a39c34c19922
moboluwade <folarinboluwade.com>
2023-12-05 00:46:35 +0100

    make AboutHero mobile responsive

---
+++ src/components/About/Context/AboutContext.jsx
@@ -0,0 +1,21 @@
+const AboutContext = () => {
+    return (
+        <div className="bg-white">
+            <div className="max-w-[87.5rem] lg:px-[7.5rem] md:px-12 px-4 flex flex-col items-center  py-14 md:flex-row md:gap-64">
+                <span>
+                    <h3 className="pb-8"><span className="bg-text-dev-orange">Welcome</span> to Dev League</h3>
+                    <span>
+                        <span>
+                            A community that blends technology with inclusivity. In a world where technology is the push behind innovation, we understand the importance of creating an environment that embraces diversity and inclusivity so whether you are a code-savvy techie, seeking to enhance your coding skills or a code-curious techie exploring the realm of no-code solutions Dev League is your space to learn, collaborate and connect.
+                        </span>
+                        <span>
+                            Here, you&apos;ll find the right resources and support to take your tech career to the next level. you&apos;ll also discover a diverse group of individuals who are passionate about technology and dedicated to fostering a supportive atmosphere for everyone, regardless of their varied backgrounds, experiences, and skill sets.  Our community is more than just a gathering of minds; it&apos;s a vibrant ecosystem thriving with mentorship, knowledge sharing, and collaboration opportunities.</span>
+                    </span>
+                </span>
+            </div>
+
+        </div>
+    )
+}
+
+export default AboutContext
--- src/components/About/Hero/AboutHero.jsx
+++ src/components/About/Hero/AboutHero.jsx
@@ -1,11 +1,13 @@
 const AboutHero = () => {
   return (
-    <div className=" lg:px-[7.5rem] md:px-12 px-4 xl:h-[25rem] lg:h-25rem bg-text-dev-light-orange flex flex-col justify-center items-center relative">
-      <div className="absolute">
-        <span className="absolute"></span>
-        <span></span>
+    <div className="flex flex-col items-center justify-center h-fit lg:h-25rem bg-text-dev-light-orange">
+      <div className="max-w-[87.6rem] w-full xl:h-[25rem] h-[12.5rem] lg:px-[7.5rem] md:px-12 px-4 flex flex-col justify-center items-center relative">
+        <div className="absolute">
+          <span className="absolute"></span>
+          <span></span>
+        </div>
+        <h1 className="md:text-6xl text-[2.1rem] text-center font-bold leading-[-0.15rem] text-black">About <span className="uppercase text-text-dev-orange">Dev League</span></h1>
       </div>
-      <h1 className="text-6xl font-bold leading-[-0.15rem] text-blac">About <span className="uppercase text-text-dev-orange">Dev League</span></h1>
     </div>
   )
 }
