bfdc4f3ef2f4bb9cedcbd17cd41184f686216a24
moboluwade <folarinboluwade.com>
2023-12-05 02:00:43 +0100

    add AboutContext images

--- src/components/About/Context/AboutContext.jsx
+++ src/components/About/Context/AboutContext.jsx
@@ -1,9 +1,25 @@
 const AboutContext = () => {
     return (
-        <div className="bg-white">
-            <div className="max-w-[87.5rem] lg:px-[7.5rem] md:px-12 px-4 flex flex-col items-center  py-14 md:flex-row md:gap-64">
+        <div className="flex flex-col items-center justify-center bg-white">
+            <div className="max-w-[87.5rem] w-full lg:px-[7.5rem] md:px-12 px-4 flex flex-col items-center  py-14 md:flex-row md:gap-64">
+                <div className="relative flex flex-row h-80">
+                    <div className="relative max-w-60">
+                        <div className="relative -left-10">
+                            <img className="top-0 object-cover rounded-lg h-44 w-44" src="/about-image-square.png" alt="dev-league-team" />
+                        </div>
+                        <div className="absolute right-0">
+                            <div className="overflow-hidden rounded-lg w-fit h-fit ">
+                                <img className=" object-cover object-top w-[8.8rem] h-20 scale-[1.5]  overflow-clip " src="/about-image-rectangle.png" alt="dev-league-event" />
+                            </div>
+                        </div>
+
+                    </div>
+                    <div className="absolute right-0">
+                        <img className="object-cover w-32 rounded-lg h-52" src="/about-image-slim.png" alt="brainstorm-session" />
+                    </div>
+                </div>
                 <span>
-                    <h3 className="pb-8"><span className="bg-text-dev-orange">Welcome</span> to Dev League</h3>
+                    <h3 className="pb-4 text-2xl font-bold"><span className="text-text-dev-orange">Welcome</span> to Dev League</h3>
                     <span>
                         <span>
                             A community that blends technology with inclusivity. In a world where technology is the push behind innovation, we understand the importance of creating an environment that embraces diversity and inclusivity so whether you are a code-savvy techie, seeking to enhance your coding skills or a code-curious techie exploring the realm of no-code solutions Dev League is your space to learn, collaborate and connect.
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -1,3 +1,4 @@
+import { AboutContext } from "../../components/About/Context"
 import { AboutHero } from "../../components/About/Hero"
 
 const About = () => {
@@ -6,6 +7,7 @@ const About = () => {
       {/* can always copy an paste these tailwind padding classes to other components for uniformity*/}
       {/*  lg:px-[7.5rem] md:px-12 px-4  */}
       <AboutHero/>
+      <AboutContext/>
     </div>
   )
 }
