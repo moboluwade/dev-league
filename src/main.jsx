6cdfd64c80fe63bd20a4eb043b8e055dd89b3156
Muideen Popoola <popmuideen@gmail.com>
2023-12-06 18:39:14 +0100

    Added the BlogView

--- src/main.jsx
+++ src/main.jsx
@@ -1,45 +1,51 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import ErrorPage from './ErrorPage.jsx'
-import About from './pages/About/About.jsx'
-import Events from './pages/Events/Events.jsx'
-import Blog from './pages/Blog/Blog.jsx'
-import Shop from './pages/Shop/Shop.jsx'
-import Home from './pages/Home/Home.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./ErrorPage.jsx";
+import About from "./pages/About/About.jsx";
+import Events from "./pages/Events/Events.jsx";
+import Blog from "./pages/Blog/Blog.jsx";
+import BlogView from "./pages/Blog/BlogView.jsx";
+import Shop from "./pages/Shop/Shop.jsx";
+import Home from "./pages/Home/Home.jsx";
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path: '/about',
+        path: "/about",
         element: <About />,
       },
       {
-        path: '/events',
+        path: "/events",
         element: <Events />,
       },
       {
-        path: '/blog',
+        path: "/blog",
         element: <Blog />,
       },
+
       {
-        path: '/shop',
+        path: "/blog/:id",
+        element: <BlogView />,
+      },
+      {
+        path: "/shop",
         element: <Shop />,
       },
     ],
   },
-])
-ReactDOM.createRoot(document.getElementById('root')).render(
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
