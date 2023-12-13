c6d6d813ffbd0ec5f9b24c3daf8223fcf64adddc
Olabasspace <126046360+Olabasspace@users.noreply.github.com>
2023-12-13 10:55:21 +0100

    Add files via upload
    BlogArticle

---
+++ dev-league-main/dev-league/src/pages/Blog/Blog.jsx
@@ -0,0 +1,14 @@
+import BlogHero from "./BlogHero";
+import "./Blog.css";
+import BlogContent from "./BlogContent/BlogContent";
+
+const Blog = (props) => {
+  return (
+    <div>
+      <BlogHero/>
+      <BlogContent />
+    </div>
+  );
+};
+
+export default Blog;
