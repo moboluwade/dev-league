e4b9b6d3c394d6c9153d0cc4f0de532885c3883e
moboluwade <folarinboluwade.com>
2023-12-15 08:59:58 +0100

    chore(Events): init EventsContext file

---
+++ src/components/Events/Context/EventsContext.jsx
@@ -0,0 +1,9 @@
+const EventsContext = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default EventsContext
--- src/components/Events/Hero/EventsHero.jsx
+++ src/components/Events/Hero/EventsHero.jsx
@@ -5,7 +5,7 @@ const EventsHero = () => {
   return (
     <div className="flex flex-col items-center justify-center overflow-hidden h-fit lg:h-fit bg-text-dev-light-orange">
       <div className="max-w-[87.6rem] w-full lg:h-[15rem] h-[12.5rem] lg:px-[7.5rem] md:px-12 px-4 flex flex-col justify-center items-center relative">
-        <div className="absolute">
+        <div className="absolute z-10">
           <div className="flex flex-row">
             <motion.div
               initial={{ y: 60 }}
@@ -46,11 +46,15 @@ const EventsHero = () => {
           className="md:text-6xl text-[2.1rem] text-center font-bold leading-[-0.15rem] text-black pb-10 ">
           <span className="uppercase text-text-dev-orange">Dev League</span> Events
         </h1>
-        <div className="flex flex-row items-center gap-4">
-          <span className="text-4xl font-bold hover:text-text-dev-orange">Categories</span>
-          <div className="border border-[#D4CECB] rounded-lg px-5 py-1 text-lg hover:text-text-dev-orange">All</div>
-          <div className="border border-[#D4CECB] rounded-lg px-5 py-1 text-lg text-text-dev-orange">Virtual</div>
-          <div className="border border-[#D4CECB] rounded-lg px-5 py-1 text-lg">Physical</div>
+        <div className="z-20 flex flex-row items-center gap-4">
+          <span className="text-4xl font-bold">Categories</span>
+          <motion.button
+          whileFocus={{color:"rgb(253,79,19)"}}
+          className="border border-[#D4CECB] rounded-lg px-5 py-1 text-lg">All</motion.button>
+          <motion.button
+          whileFocus={{color:"rgb(253,79,19)"}}
+          className="border border-[#D4CECB] rounded-lg px-5 py-1 text-lg">Virtual</motion.button>
+          <button className="border border-[#D4CECB] rounded-lg px-5 py-1 text-lg focus:text-text-dev-orange">Physical</button>
           <div className="border border-[#D4CECB] rounded-lg px-5 py-1 text-lg">Upcoming</div>
           <div className="border border-[#D4CECB] rounded-lg px-5 py-1 text-lg">Past</div>
         </div>
