6f55d8fe27409753e28c61011a0a17cc9b7b76ec
adebabs-tec <ruqoyatomotayo@gmail.com>
2023-12-21 20:36:35 +0100

    donate section again

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,8 @@
-import { Sponsor } from "../../components/Sponsor";
-import { Hero } from "../../components/Hero";
-import { Donation } from "../../components/Donation";
-import { Impact } from "../../components/Impact";
-import { Articles } from "../../components/Articles";
+import { Sponsor } from '../../components/Sponsor'
+import { Hero } from '../../components/Hero'
+import { Donation } from '../../components/Donation'
+import { Impact } from '../../components/Impact'
+import { Articles } from '../../components/Articles'
 const Home = () => {
   return (
     <>
@@ -10,7 +10,9 @@ const Home = () => {
       {/* max-w-[88rem] lg:px-[7.5rem] md:px-12 px-4  */}
       <Hero />
       <Sponsor />
-      <Donation />
+      <section id="donationSection">
+        <Donation />
+      </section>
       <Impact />
       <Articles />
     </>
