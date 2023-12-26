ba4d7e5e5f0b450a9d522830902aaa433705ba75
adebabs-tec <ruqoyatomotayo@gmail.com>
2023-12-26 18:47:59 +0100

    events page contd

--- src/pages/EventsPage/Card/CardDisplay.jsx
+++ src/pages/EventsPage/Card/CardDisplay.jsx
@@ -1,15 +1,29 @@
 import Card from './Card'
-import { eventsData } from '../data'
 
-const CardDisplay = () => {
+const CardDisplay = ({ displayType, eventsData, setEventsData }) => {
+  let displayedCards = eventsData
+
+  if (displayType === 'upcoming') {
+    displayedCards = eventsData.filter((card) => card.isEventOpen)
+  } else if (displayType === 'closed') {
+    displayedCards = eventsData.filter((card) => !card.isEventOpen)
+  }
+
+  const handleCardClick = (cardId) => {
+    const updatedCards = eventsData.map((card) =>
+      card.id === cardId ? { ...card, isEventOpen: !card.isEventOpen } : card,
+    )
+    setEventsData(updatedCards)
+  }
   return (
     <div className="grid lg:grid-cols-2 grid-cols-1 gap-4">
-      {eventsData.map((card) => (
+      {displayedCards.map((card) => (
         <div key={card.id}>
           <Card
             isEventOpen={card.isEventOpen}
             day={card.day}
             month={card.month}
+            onClick={handleCardClick}
           />
         </div>
       ))}
