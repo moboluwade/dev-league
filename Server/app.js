ec74ae027795e6216c643e80eb61076144800050
SIta Dabo <94798285+Obad0x@users.noreply.github.com>
2024-01-25 15:37:35 +0100

    added mongoose and mongosb

--- Server/app.js
+++ Server/app.js
@@ -3,13 +3,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT;
 const admin = require('./routes/client');
-const ConnectDB = require('./config/ConnectDb')
+const client = require("./routes/client");
+const ConnectDB = require('./config/ConnectDb');
 
 
-ConnectDB()
-app.use(express.json())
+ConnectDB();
+app.use(express.json());
 
-app.use(admin)
+app.use(admin);
+app.use(client);
 
 
 
