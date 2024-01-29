3caf1a46680ac20f464138c4ac460e614726a39c
SIta Dabo <94798285+Obad0x@users.noreply.github.com>
2024-01-29 03:30:30 +0100

    integrarion for the single time payment
    added paystack test keys and tested it , it resturns an authorised url where payment can be made securely through paystack

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,8 @@ const app = express();
 const ConnectDB = require('./config/ConnectDb');
 const email = require('./routes/email');
 const port = process.env.PORT || 5000;
-const events =require('./routes/events')
+const events = require('./routes/events');
+const donations = require('./routes/donation')
 
 
 ConnectDB()
@@ -12,7 +13,8 @@ ConnectDB()
 
 app.use(express.json())
 app.use('/api/emails', email)
-app.use('/api/events', events)
+app.use('/api/events', events);
+app.use('/api/donations', donations)
 
 
 
