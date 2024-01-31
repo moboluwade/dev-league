43054db584457c0c6eb80b26d80c2da01e69cf7a
Muideen Popoola <popoolamuideen61@gmail.com>
2024-01-31 06:41:28 +0100

    Added the login page

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,48 +1,52 @@
-import { Link, NavLink, useNavigate } from 'react-router-dom'
-import './Header.css'
-import { motion } from 'framer-motion'
-import { FiBarChart } from 'react-icons/fi'
-import { RxCrossCircled } from 'react-icons/rx'
-import { useState } from 'react'
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import "./Header.css";
+import { motion } from "framer-motion";
+import { FiBarChart } from "react-icons/fi";
+import { RxCrossCircled } from "react-icons/rx";
+import { useState } from "react";
 
 const Header = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const handleDonateClick = () => {
-    navigate('/')
+    navigate("/");
     setTimeout(() => {
-      const donationSection = document.getElementById('donationSection')
+      const donationSection = document.getElementById("donationSection");
       if (donationSection) {
         // Scroll to the donation section
-        donationSection.scrollIntoView({ behavior: 'smooth' })
+        donationSection.scrollIntoView({ behavior: "smooth" });
       }
-    }, 100)
-  }
+    }, 100);
+  };
 
   const links = [
     {
-      title: 'About us',
-      path: '/about',
+      title: "Home",
+      path: "/",
     },
     {
-      title: 'Events',
-      path: '/events',
+      title: "About us",
+      path: "/about",
     },
     {
-      title: 'Blog',
-      path: '/blog',
+      title: "Events",
+      path: "/events",
     },
     {
-      title: 'Shop',
-      path: '/shop',
+      title: "Blog",
+      path: "/blog",
     },
-  ]
+    {
+      title: "Shop",
+      path: "/shop",
+    },
+  ];
 
-  const [togglerNav, setTogglerNav] = useState(false)
+  const [togglerNav, setTogglerNav] = useState(false);
 
   const handleClick = () => {
-    setTogglerNav(!togglerNav)
-    console.log(togglerNav)
-  }
+    setTogglerNav(!togglerNav);
+    console.log(togglerNav);
+  };
 
   return (
     <div className="bg-white flex flex-row justify-center text lg:px-[7.5rem] md:px-8 px-auto">
@@ -61,7 +65,7 @@ const Header = () => {
               <NavLink to={link.path} key={link.title} end>
                 {link.title}
               </NavLink>
-            )
+            );
           })}
         </div>
         <div className="flex flex-row items-center gap-4">
@@ -86,9 +90,12 @@ const Header = () => {
               className="px-3 py-3 rounded-lg md:px-5 md:py-3 border-2 border-text-dev-orange"
               // onClick={handleLoginClick}
             >
-              <span className="text-sm font-semibold text-dev-orange md:text-xl">
+              <Link
+                to="/admin/login"
+                className="text-sm font-semibold text-dev-orange md:text-xl"
+              >
                 Login
-              </span>
+              </Link>
             </motion.button>
           </div>
 
@@ -100,9 +107,9 @@ const Header = () => {
             {!togglerNav && (
               <FiBarChart
                 style={{
-                  transform: 'rotate(270deg)',
-                  color: '#fd4f13',
-                  fontSize: '3rem',
+                  transform: "rotate(270deg)",
+                  color: "#fd4f13",
+                  fontSize: "3rem",
                 }}
                 className="top-8"
               />
@@ -119,7 +126,7 @@ const Header = () => {
         {/* mobile navigation menu */}
         <motion.div
           className={`md:hidden absolute inset-y-0 h-[574px] w-[15rem] z-50 bg-white p-4 transition duration-300 transform ${
-            togglerNav ? 'translate-x-0 top-0 right-0' : 'hidden'
+            togglerNav ? "translate-x-0 top-0 right-0" : "hidden"
           }`}
         >
           {/* Navbar content goes here */}
@@ -128,13 +135,13 @@ const Header = () => {
               <h1 className="font-semibold text-xl/[19px]">Main Menu</h1>
               <RxCrossCircled
                 className="cursor-pointer"
-                style={{ fontSize: '2rem', color: '#D4CECB' }}
+                style={{ fontSize: "2rem", color: "#D4CECB" }}
                 onClick={() => setTogglerNav(!togglerNav)}
               />
             </div>
-            <NavLink onClick={handleClick} to={'/'} key={'Home'} end>
+            <NavLink onClick={handleClick} to={"/"} key={"Home"} end>
               <div className="flex items-center justify-between py-4 mt-3 border-b-2">
-                {'Home'}
+                {"Home"}
                 <img src="/Vector.png" alt="vector" className="vec" />
               </div>
             </NavLink>
@@ -151,13 +158,13 @@ const Header = () => {
                     <img src="/Vector.png" alt="vector" className="vec" />
                   </div>
                 </NavLink>
-              )
+              );
             })}
           </div>
         </motion.div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,58 +1,63 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import ErrorPage from './ErrorPage.jsx'
-import About from './pages/About/About.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./ErrorPage.jsx";
+import About from "./pages/About/About.jsx";
 // import Events from './pages/EventsPage/Events.jsx'
-import Events from './pages/Events/Events.jsx'
-import EventView from './pages/Events/EventView/EventView.jsx'
-import Blog from './pages/Blog/Blog.jsx'
-import BlogView from './pages/Blog/BlogView.jsx'
-import Shop from './pages/Shop/Shop.jsx'
-import Home from './pages/Home/Home.jsx'
+import Events from "./pages/Events/Events.jsx";
+import EventView from "./pages/Events/EventView/EventView.jsx";
+import Blog from "./pages/Blog/Blog.jsx";
+import BlogView from "./pages/Blog/BlogView.jsx";
+import Shop from "./pages/Shop/Shop.jsx";
+import Home from "./pages/Home/Home.jsx";
+import Login from "./components/admin/login/login.jsx";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
     children: [
       {
-        path: '/',
+        path: "/",
         element: <Home />,
       },
       {
-        path: '/about',
+        path: "/about",
         element: <About />,
       },
       {
-        path: '/events',
+        path: "/events",
         element: <Events />,
       },
       {
-        path: '/event-details',
+        path: "/event-details",
         element: <EventView />,
       },
       {
-        path: '/blog',
+        path: "/blog",
         element: <Blog />,
       },
 
       {
-        path: '/blog/:id',
+        path: "/blog/:id",
         element: <BlogView />,
       },
       {
-        path: '/shop',
+        path: "/shop",
         element: <Shop />,
+      },
+      {
+        path: "/admin/login",
+        element: <Login />,
       },
     ],
   },
-])
-ReactDOM.createRoot(document.getElementById('root')).render(
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>,
-)
+);
