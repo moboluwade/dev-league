f575d36a84c10ef5caf53e9c24abffe5ac87a355
moboluwade <folarinboluwade.com>
2024-02-02 03:58:58 +0100

    chore: complete base designs for events page

--- client/src/components/admin/event/Event.jsx
+++ client/src/components/admin/event/Event.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { MarkCalendar } from "../svg";
 
 const Event = () => {
     const [selectedLocation, setSelectedLocation] = useState(null);
@@ -42,6 +43,42 @@ const Event = () => {
                             />
                             Onsite
                         </label>
+                    </div>
+                    <div className="pb-8"></div>
+                    <div className="flex flex-col pt-4">
+                        <label className="pb-1 font-bold" htmlFor="event-title">Event Start Date</label>
+                        <div className=" h-fit border-[1.5px] bg-[#E2DEDC] border-[#292422] rounded-md flex flex-row w-[36rem]">
+                            <input className="bg-[#E2DEDC] placeholder:text-[#9F918B] pl-4 h-12 outline-none rounded-md w-[88%] " placeholder="Event Title" type="text" name="event-title" id="event-title" />
+                            <button className="w-6 px-6 border-l border-black"><MarkCalendar /></button>
+                        </div>
+                    </div>
+                    <div className="flex flex-col pt-4">
+                        <label className="pb-1 font-bold" htmlFor="event-title">Event Start Date</label>
+                        <div className=" h-fit border-[1.5px] bg-[#E2DEDC] border-[#292422] rounded-md flex flex-row w-[36rem]">
+                            <input className="bg-[#E2DEDC] placeholder:text-[#9F918B] pl-4 h-12 outline-none rounded-md w-[88%] " placeholder="Event Title" type="text" name="event-title" id="event-title" />
+                            <button className="w-6 px-6 border-l border-black"><MarkCalendar /></button>
+                        </div>
+                    </div>
+                    <div className="flex flex-col pt-4">
+                        <label className="pb-1 font-bold" htmlFor="event-title">Event Start Date</label>
+                        <div className=" h-fit border-[1.5px] bg-[#E2DEDC] border-[#292422] rounded-md flex flex-row w-[36rem]">
+                            <input className="bg-[#E2DEDC] placeholder:text-[#9F918B] pl-4 h-12 outline-none rounded-md w-[88%] " placeholder="Event Title" type="text" name="event-title" id="event-title" />
+                            <button className="w-6 px-6 border-l border-black"><MarkCalendar /></button>
+                        </div>
+                    </div>
+                    <div className="flex flex-col pt-4">
+                        <label className="pb-1 font-bold" htmlFor="event-title">Event Start Date</label>
+                        <div className=" h-fit border-[1.5px] bg-[#E2DEDC] border-[#292422] rounded-md flex flex-row w-[36rem]">
+                            <input className="bg-[#E2DEDC] placeholder:text-[#9F918B] pl-4 h-12 outline-none rounded-md w-[88%] " placeholder="Event Title" type="text" name="event-title" id="event-title" />
+                            <button className="w-6 px-6 border-l border-black"><MarkCalendar /></button>
+                        </div>
+                    </div>
+                    <div className="flex flex-col pt-4">
+                        <label className="pb-1 font-bold" htmlFor="event-title">Event Start Date</label>
+                        <div className=" h-fit border-[1.5px] bg-[#E2DEDC] border-[#292422] rounded-md flex flex-row w-[36rem]">
+                            <input className="bg-white placeholder:text-[#9F918B] pl-4 h-12 outline-none rounded-md rounded-r-none min-w-[88%] " placeholder="Event Title" type="text" name="event-title" id="event-title" />
+                            <button className="flex flex-row content-center justify-center w-full border-l border-black"><span className="m-auto font-bold text-black w-fit h-fit">Browse</span></button>
+                        </div>
                     </div>
                 </div>
             </div>
