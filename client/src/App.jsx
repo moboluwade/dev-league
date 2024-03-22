1036e340b72a0aa2049a87dabb439427a1a9feb2
moboluwade <folarinboluwade.com>
2024-03-22 21:18:44 +0100

    chore: implement navigation logic with redux

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,32 +2,17 @@ import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header'
 import { Footer } from './components/Footer'
-import { useSelector } from 'react-redux'
-import SideNavbar from './components/Header/SideNavbar'
-import AllPost from './pages/admin/AllPost/AllPost'
 
 const App = () => {
-  const isLoggedIn = useSelector((state) => state.user.isLoggedIn)
-  console.log(isLoggedIn)
+
   return (
     <>
       <div className="min-h-screen">
-        {!isLoggedIn && (
-          <div>
-            <Header />
-            <Outlet />
-            <Footer />
-          </div>
-        )}
-        {isLoggedIn && (
-          <div>
-            <div className="flex">
-              <SideNavbar />
-              <AllPost />
-            </div>
-            <Footer />
-          </div>
-        )}
+        <div>
+          <Header />
+          <Outlet />
+          <Footer />
+        </div>
       </div>
     </>
   )
--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -1,16 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import { Navbar } from "./Navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux"
 
 const Admin = () => {
     // this boolean logic should only affect mobile
     const [navActive, setNavActive] = useState(false)
+    const isLoggedIn = useSelector(state => state.user.isLoggedIn)
+
+    const navigate = useNavigate()
+    useEffect(() => {
+        !isLoggedIn && navigate("/admin/login")
+    }, [isLoggedIn, navigate])
+
 
     return (
         <div className="flex flex-col w-full h-screen overflow-y-scroll md:flex-row">
             <Navbar setNavActive={setNavActive} navActive={navActive} />
             <button
-                className={`${navActive? "hidden" : "relative top-4 left-4 pb-12" } md:hidden`}
+                className={`${navActive ? "hidden" : "relative top-4 left-4 pb-12"} md:hidden`}
                 onClick={() => setNavActive(!navActive)}
             >
                 <img className="p-2 bg-[#FD4F13] rounded-full w-fit h-fit " src="/admin/menu.svg" alt="menu" />
