87ae45d471e4a282c6f8af5ac0c9f6f6f5e6cac8
moboluwade <folarinboluwade.com>
2024-03-26 10:34:50 +0100

    chore: handle delete type logic

--- client/src/pages/admin/CreateBlog/CreateBlog.jsx
+++ client/src/pages/admin/CreateBlog/CreateBlog.jsx
@@ -1,14 +1,39 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import DateStart from "../DateStart"
 
 const CreateBlog = () => {
-    const [selectedLocation, setSelectedLocation] = useState(null)
+    // 
+    const [selectedBlogTypes, setSelectedBlogTypes] = useState(["articles", "jobs", "news", "tech"])
+    //
+    const handleTypeChange = (event) => {
+        const typeToRemove = event.target.value
+        setSelectedBlogTypes(prev => {
+            // function removes existing types and adds new types
+            const filter = (previous, typeToRemove) => {
+                if (previous.includes(typeToRemove)) {
+                    // prev is previous value in setState, which is a list.
+                    const newArray = prev.filter((type) => { type !== typeToRemove })
+                    return newArray
+                    // is a new array of types.
+                } else {
+                    return [...previous, typeToRemove]
+                }
+            }
 
-    const handleLocationChange = (event) => {
-        setSelectedLocation(event.target.value)
+            filter(prev, typeToRemove)
+        })
     }
 
+    const handleTypeDelete = (typeToRemove) => {
+        //remove type from array
+        const array = selectedBlogTypes;
+        const newArray = array.filter((type) => { return type !== typeToRemove })
+        setSelectedBlogTypes(newArray)
+    }
 
+useEffect(()=>{
+    console.log(selectedBlogTypes)
+},[selectedBlogTypes])
 
     return (
         <div className="w-full ">
@@ -25,51 +50,67 @@ const CreateBlog = () => {
                     </div>
                     <div className="flex flex-col pt-4">
                         <label className="pb-1 font-bold" htmlFor="blog-type">Blog Type</label>
-                        <div className="flex flex-row justify-center md:justify-start content-center border-[1.5px] border-[#292422] placeholder:text-[#9F918B] pl-4 py-4 w-full max-w-[36rem] placeholder:text-start h-fit gap-6 outline-none rounded-md overflow-scroll" name="blog-type" id="blog-type" >
-                            <label className={`px-3 flex flex-row items-center gap-1 pt-[0.125rem] pb-[0.125rem] text-white border h-fit rounded-2xl ${selectedLocation === "virtual" ? "bg-text-dev-orange" : "bg-[#D4CECB] text-[#9F918B]"} `}>
-                                <input
-                                    className={`w-2 h-2 bg-white rounded-full appearance-none  ${selectedLocation === "virtual" ? "bg-white" : "bg-[#9F918B]"}`}
-                                    type="radio"
-                                    value="virtual"
-                                    checked={selectedLocation === "virtual"}
-                                    onChange={handleLocationChange}
-                                />
-                                <span>Virtual</span>
-                            </label>
-                            <label className={`px-3 flex flex-row items-center gap-1 pt-[0.125rem] pb-[0.125rem] text-white border h-fit rounded-2xl ${selectedLocation === "onsite" ? "bg-text-dev-orange" : "bg-[#D4CECB] text-[#9F918B]"}`}>
-                                <input
-                                    className={`w-2 h-2 bg-white rounded-full appearance-none  ${selectedLocation === "onsite" ? "bg-white" : "bg-[#9F918B]"}`}
-                                    type="radio"
-                                    value="onsite"
-                                    checked={selectedLocation === "onsite"}
-                                    onChange={handleLocationChange}
-                                />
-                                <span>
-                                    Onsite
-                                </span>
-                            </label>
-                            <label className={`px-3 flex flex-row items-center gap-1 pt-[0.125rem] pb-[0.125rem] text-white border h-fit rounded-2xl ${selectedLocation === "virtual" ? "bg-text-dev-orange" : "bg-[#D4CECB] text-[#9F918B]"} `}>
-                                <input
-                                    className={`w-2 h-2 bg-white rounded-full appearance-none  ${selectedLocation === "virtual" ? "bg-white" : "bg-[#9F918B]"}`}
-                                    type="radio"
-                                    value="virtual"
-                                    checked={selectedLocation === "virtual"}
-                                    onChange={handleLocationChange}
-                                />
-                                <span>Virtual</span>
-                            </label>
-                            <label className={`px-3 flex flex-row items-center gap-1 pt-[0.125rem] pb-[0.125rem] text-white border h-fit rounded-2xl ${selectedLocation === "onsite" ? "bg-text-dev-orange" : "bg-[#D4CECB] text-[#9F918B]"}`}>
-                                <input
-                                    className={`w-2 h-2 bg-white rounded-full appearance-none  ${selectedLocation === "onsite" ? "bg-white" : "bg-[#9F918B]"}`}
-                                    type="radio"
-                                    value="onsite"
-                                    checked={selectedLocation === "onsite"}
-                                    onChange={handleLocationChange}
-                                />
-                                <span>
-                                    Onsite
-                                </span>
-                            </label>
+                        <div className="flex flex-row justify-center md:justify-start content-center border-[1.5px] border-[#292422] placeholder:text-[#9F918B] pl-4 py-4 w-full max-w-[36rem] placeholder:text-start h-fit gap-6 outline-none rounded-md overflow-x-scroll" name="blog-type" id="blog-type" >
+                            <div className="flex flex-row gap-2">
+                                <label className={`px-3 flex flex-row items-center gap-1 pt-[0.125rem] pb-[0.125rem] text-white border h-fit rounded-2xl ${selectedBlogTypes.includes("article") ? "bg-text-dev-orange" : "bg-[#D4CECB] text-[#9F918B]"} `}>
+                                    <input
+                                        className={`w-2 h-2 bg-white rounded-full appearance-none  ${selectedBlogTypes.includes("articles") ? "bg-white" : "bg-[#9F918B]"}`}
+                                        type="checkbox"
+                                        value="articles"
+                                        checked={selectedBlogTypes === "articles"}
+                                        onChange={(e) => handleTypeChange(e)}
+                                    />
+                                    <span>Article</span>
+                                </label>
+                                <img onClick={() => {handleTypeDelete("articles") }} src="/admin/delete.svg" alt="delete" />
+                            </div>
+
+                            <div className="flex flex-row gap-2">
+                                <label className={`px-3 flex flex-row items-center gap-1 pt-[0.125rem] pb-[0.125rem] text-white border h-fit rounded-2xl ${selectedBlogTypes.includes("news") ? "bg-text-dev-orange" : "bg-[#D4CECB] text-[#9F918B]"}`}>
+                                    <input
+                                        className={`w-2 h-2 bg-white rounded-full appearance-none  ${selectedBlogTypes.includes("news") ? "bg-white" : "bg-[#9F918B]"}`}
+                                        type="checkbox"
+                                        value="news"
+                                        checked={selectedBlogTypes === "news"}
+                                        onChange={handleTypeChange}
+                                    />
+                                    <span>
+                                        News
+                                    </span>
+                                </label>
+                                <img  onClick={() => { handleTypeDelete("news") }} src="/admin/delete.svg" alt="delete" />
+                            </div>
+
+                            <div className="flex flex-row gap-2">
+                                <label className={`px-3 flex flex-row items-center gap-1 pt-[0.125rem] pb-[0.125rem] text-white border h-fit rounded-2xl ${selectedBlogTypes.includes("tech") ? "bg-text-dev-orange" : "bg-[#D4CECB] text-[#9F918B]"} `}>
+                                    <input
+                                        className={`w-2 h-2 bg-white rounded-full appearance-none  ${selectedBlogTypes.includes("tech") ? "bg-white" : "bg-[#9F918B]"}`}
+                                        type="checkbox"
+                                        value="tech"
+                                        checked={selectedBlogTypes === "tech"}
+                                        onChange={handleTypeChange}
+                                    />
+                                    <span>Tech</span>
+                                </label>
+                                <img onClick={() => { handleTypeDelete("tech") }} src="/admin/delete.svg" alt="delete" />
+                            </div>
+
+
+                            <div className="flex flex-row gap-2">
+                                <label className={`px-3 flex flex-row items-center gap-1 pt-[0.125rem] pb-[0.125rem] text-white border h-fit rounded-2xl ${selectedBlogTypes.includes("jobs") ? "bg-text-dev-orange" : "bg-[#D4CECB] text-[#9F918B]"}`}>
+                                    <input
+                                        className={`w-2 h-2 bg-white rounded-full appearance-none  ${selectedBlogTypes.includes("jobs") ? "bg-white" : "bg-[#9F918B]"}`}
+                                        type="checkbox"
+                                        value="jobs"
+                                        checked={selectedBlogTypes === "jobs"}
+                                        onChange={handleTypeChange}
+                                    />
+                                    <span>
+                                        Jobs
+                                    </span>
+                                </label>
+                                <img  onClick={() => { handleTypeDelete("jobs") }} src="/admin/delete.svg" alt="delete" />
+                            </div>
                         </div>
                         <div className="pb-8"></div>
 
