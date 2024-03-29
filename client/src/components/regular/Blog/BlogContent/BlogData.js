36697ad11c10243a4073c14b03ede741a3f5f9d5
moboluwade <folarinboluwade.com>
2024-03-29 16:28:59 +0100

    style: give pagination buttons a light orange background

--- client/src/components/regular/Blog/BlogContent/BlogData.js
+++ client/src/components/regular/Blog/BlogContent/BlogData.js
@@ -60,6 +60,42 @@ export default [
         button: 'News'
     },
     {
+      id: 7,
+      image: '/Image (1).png',
+      heading: 'The Impact of DevOps on Software Development and Deployment',
+      body: 'How do you create compelling presentations that wow your audience ...',
+      author: {
+        img: '/Avatar.png',
+        name: 'Azeez',
+        date: '20 Feb 2023',
+      },
+      button: 'News'
+  },
+  {
+    id: 8,
+    image: '/Image (1).png',
+    heading: 'The Impact of DevOps on Software Development and Deployment',
+    body: 'How do you create compelling presentations that wow your audience ...',
+    author: {
+      img: '/Avatar.png',
+      name: 'Azeez',
+      date: '20 Feb 2023',
+    },
+    button: 'News'
+},
+{
+  id: 9,
+  image: '/Image (1).png',
+  heading: 'The Impact of DevOps on Software Development and Deployment',
+  body: 'How do you create compelling presentations that wow your audience ...',
+  author: {
+    img: '/Avatar.png',
+    name: 'Azeez',
+    date: '20 Feb 2023',
+  },
+  button: 'News'
+},
+    {
         id: 6,
         image: '/Image.png',
         heading: 'The Impact of DevOps on Software Development and Deployment',
