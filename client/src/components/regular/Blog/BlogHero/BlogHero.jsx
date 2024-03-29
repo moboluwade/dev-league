c8f9035515f26c5abd063da2b8b7dce8258075a1
moboluwade <folarinboluwade.com>
2024-03-29 15:53:49 +0100

    style: adjust blog header

--- client/src/components/regular/Blog/BlogHero/BlogHero.jsx
+++ client/src/components/regular/Blog/BlogHero/BlogHero.jsx
@@ -7,7 +7,7 @@ const BlogHero = () => {
 
   return (
     <div className="flex flex-col items-center justify-center overflow-hidden h-fit lg:h-fit bg-text-dev-light-orange">
-      <div className="max-w-[87.6rem] w-full lg:h-[15rem] h-[12.5rem] lg:px-[7.5rem] md:px-12 px-4 md:pt-16 lg:pt-8 flex flex-col justify-center items-center relative">
+      <div className="max-w-[87.6rem] w-full lg:h-fit h-fit lg:px-[7.5rem] md:px-12 px-4 md:pt-16 lg:py-12 py-8 flex flex-col justify-center items-center relative">
         <div className="absolute z-10">
           <div className="flex flex-row">
             <motion.div
@@ -52,7 +52,7 @@ const BlogHero = () => {
 
           <div className="flex flex-row justify-center w-full gap-2 pt-6 ">
             <input
-              className="cursor-pointer border-[1.5px] border-[#8E8782] outline-none h-12 md:w-[30rem] rounded-md text-sm md:p-4 w-48 p-1"
+              className="cursor-pointer border-[1.5px] p-1 pl-14 border-[#8E8782] outline-none h-12 md:w-[30rem] rounded-md text-sm md:p-4 w-48 "
               value={search}
               onChange={(e) => {
                 setSearch(e.target.value);
@@ -67,16 +67,16 @@ const BlogHero = () => {
         <div className="flex flex-row font-semibold md:font-bold pt-6 font-[inter] justify-center gap-3 sm:gap-5">
           <span className="md:text-4xl text-[20px] ">Categories</span>
           <div className="flex gap-2 text-xs cursor-pointer md:text-xl">
-            <span className="py-[4px] px-[6px] border-2 font-['Manrope'] rounded-[5px]">
+            <span className="py-[4px] px-4 border-2 font-['Manrope'] rounded-lg">
               All
             </span>
-            <span className="py-[4px] px-[6px] border-2 font-['Manrope'] rounded-[5px]">
+            <span className="py-[4px] px-4 border-2 font-['Manrope'] rounded-lg">
               Articles
             </span>
-            <span className="py-[4px] px-[6px] border-2 font-['Manrope'] rounded-[5px]">
+            <span className="py-[4px] px-4 border-2 font-['Manrope'] rounded-lg">
               News
             </span>
-            <span className="py-[4px] px-[6px] border-2 font-['Manrope'] rounded-[5px]">
+            <span className="py-[4px] px-4 border-2 font-['Manrope'] rounded-lg">
               Jobs
             </span>
           </div>
