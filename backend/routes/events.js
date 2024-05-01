901519c2239f400e837b6b33d76306009cbb25ff
moboluwade <folarinboluwade.com>
2024-05-01 15:19:58 +0100

    style(backend): white space formatting

--- backend/routes/events.js
+++ backend/routes/events.js
@@ -6,87 +6,85 @@ const checkAuth = require('../middlewares/auth');
 
 
 // get all the EVENTS
-router.get('/', async (req, res)=>{
+router.get('/', async (req, res) => {
     try {
 
         const Event = await Events.find()
-        .sort({date : -1});
-        res.status(200).json({ data : [ { Events : Event} ] })
-    
-        
+            .sort({ date: -1 });
+        res.status(200).json({ Events: Event })
+
     } catch (error) {
-        res.status(500).json({error : error.message}) 
+        res.status(500).json({ error: error.message })
     }
- 
 })
 
 
 // get specific event
-router.get('/:id', async  (req, res)=>{
-   
-        try {
-            const id =  req.params.id
-            const Event = await Events.findOne({_id : id})
-            res.status(200).json({ data : {event : Event} })
-            
-        } catch (error) {
-                res.status(500).json({error : error.message}) 
-        }
+router.get('/:id', async (req, res) => {
+
+    try {
+        const id = req.params.id
+        const Event = await Events.findOne({ _id: id })
+        res.status(200).json({ data: { event: Event } })
+
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
 })
 
 // CREATE A NEW EVENT 
-router.post('/', checkAuth,  async (req, res)=>{
+router.post('/', checkAuth, async (req, res) => {
     const { date, title, description, eventType, eventStatus } = req.body;
 
     try {
         const event = new Events({
-            date : date,
-            title : title,
-            description : description,
-            eventType : eventType,
-            eventStatus : eventStatus
+            date: date,
+            title: title,
+            description: description,
+            eventType: eventType,
+            eventStatus: eventStatus
         })
-        const savedEvent = await  Events.create(event);
-        res.status(201).json({ message: 'Events created successfully' ,  Event : savedEvent});
+        const savedEvent = await Events.create(event);
+        res.status(201).json({ message: 'Events created successfully', Event: savedEvent });
     } catch (error) {
-        res.status(400).json({ error: error.message, message : "Events can either be VIRTUAL or ONSITE" });
+        res.status(400).json({ error: error.message, message: "Events can either be VIRTUAL or ONSITE" });
     }
 
 })
 
 // update a specific event
-router.patch('/:id', checkAuth , async (req, res)=>{
+router.patch('/:id', checkAuth, async (req, res) => {
     try {
         const id = req.params.id;
         const update = req.body;
-        const options = {new : true}
+        const options = { new: true }
         const result = await Events.findByIdAndUpdate(id, update, options);
-        res.status(200).json({message : 'Event updated successfully', event : result})
+        res.status(200).json({ message: 'Event updated successfully', event: result })
     } catch (error) {
-        res.status(400).json({error : error.message})
+        res.status(400).json({ error: error.message })
     }
 })
 
 // GET EVENTS BY EVENT TYPE (})
 
-router.get('/eventtype/:eventType', async (req, res)=>{
+router.get('/eventtype/:eventType', async (req, res) => {
     try {
         const eventType = req.params.eventType;
-        const Event = await Events.find({eventType : eventType});
-        res.status(200).json({Events : Event})
+        const Event = await Events.find({ eventType: eventType });
+        res.status(200).json({ Events: Event })
     } catch (error) {
-        res.status(500).json({error : error.message})
+        res.status(500).json({ error: error.message })
     }
 })
 
 // GET EVENTS BY EVENT STATUS
-router.get('/eventstatus/:eventStatus', async (req, res)=>{
+router.get('/eventstatus/:eventStatus', async (req, res) => {
     try {
         const eventStatus = req.params.eventStatus;
-        const Event = await Events.find({eventStatus : eventStatus});
-        res.status(200).json({Events : Event})
+        const Event = await Events.find({ eventStatus: eventStatus });
+        res.status(200).json({ Events: Event })
     } catch (error) {
-        res.status(500).json({error : error.message})
+        res.status(500).json({ error: error.message })
     }
 })
 
