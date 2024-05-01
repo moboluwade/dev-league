ec7ef856f0763153c6530cb03ad5cda6d37aef02
moboluwade <folarinboluwade.com>
2024-05-01 15:19:11 +0100

    build(dependencies): install nodemon as backend dependency

--- backend/config/ConnectDb.js
+++ backend/config/ConnectDb.js
@@ -2,15 +2,17 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 
-async function ConnectDB(){
+async function ConnectDB() {
 
-try {
-    mongoose.connect(process.env.MONGO_URI);
-    console.log('Db COnnected Successfully')
-} catch (error) {
-    res.send(error.message).status(500);
-    console.log(error)
-}
+    try {
+        mongoose.connect(process.env.MONGO_URI,
+
+        );
+        console.log('Db COnnected Successfully')
+    } catch (error) {
+        res.send(error.message).status(500);
+        console.log(error)
+    }
 }
 
 module.exports = ConnectDB
