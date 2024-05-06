ce137cb948e02b98ee51a1706db755664454c58d
moboluwade <folarinboluwade.com>
2024-05-06 21:35:04 +0100

    feature(schema): add startDate and endDate to the event schema

--- backend/config/schema/Event.js
+++ backend/config/schema/Event.js
@@ -2,12 +2,13 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
-    date : { type : Date,  required : true },
-    title : { type : String, required : true},
-    description: { type: String, require : true },
+    startDate: { type: Date, required: true },
+    EndDate: { type: Date, required: true },
+    title: { type: String, required: true },
+    description: { type: String, require: true },
     eventType: { type: String, enum: ['virtual', 'onsite'], required: true },
-    eventStatus: { type: String, enum: ['open', 'closed'], required: true , default : 'open'}
-
-}, {timestamps : true})
+    eventStatus: { type: String, enum: ['open', 'closed'], required: true, default: 'open' },
+    eventBanner: { type: Buffer, required: false }
+}, { timestamps: true })
 
 module.exports = eventSchema
