0adda17567dc716108f29ef00fee84f034ed3c82
moboluwade <folarinboluwade.com>
2024-05-11 11:07:57 +0100

    refactor(env): add frontend cors origin as process dot env

--- backend/index.js
+++ backend/index.js
@@ -34,7 +34,7 @@ app.use(express.json({ limit: '50mb' }));
 
 app.use(cors(
     {
-        origin: 'http://localhost:5173',
+        origin: process.env.FRONTEND_URL,
         credentials: true
     }
 ))
