dac17e07e499688e555fbbc1d1a72c30472d259c
moboluwade <folarinboluwade.com>
2024-05-11 10:05:44 +0100

    refactor(schema): redefine event schema

--- backend/config/schema/Event.js
+++ backend/config/schema/Event.js
@@ -2,13 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const eventSchema = new Schema({
-    startDate: { type: Date, required: true },
-    EndDate: { type: Date, required: true },
-    title: { type: String, required: true },
-    description: { type: String, require: true },
-    eventType: { type: String, enum: ['virtual', 'onsite'], required: true },
-    eventStatus: { type: String, enum: ['open', 'closed'], required: true, default: 'open' },
-    eventBanner: { type: Buffer, required: false }
+    startDate: { type: Date },
+    endDate: { type: Date },
+    title: { type: String },
+    description: { type: String },
+    eventType: { type: String, enum: ['virtual', 'onsite'] },
+    imageUrl: { type: String },
 }, { timestamps: true })
 
 module.exports = eventSchema
