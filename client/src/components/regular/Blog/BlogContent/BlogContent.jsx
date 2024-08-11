c2c715141213f45fab1716a4e24c84126926512d
moboluwade <folarinboluwade.com>
2024-08-11 19:14:36 +0100

    fix: when no blogs exist removve superblog

--- client/src/components/regular/Blog/BlogContent/BlogContent.jsx
+++ client/src/components/regular/Blog/BlogContent/BlogContent.jsx
@@ -42,7 +42,7 @@ const BlogsToDisplay = ({ blogs }) => {
 
                   <a
                     className="underline text-text-dev-orange hover:opacity-70"
-                    href={`/blog/`+blog._id}
+                    href={`/blog/` + blog._id}
                   >
                     Read more
                   </a>
@@ -105,58 +105,61 @@ function BlogContent() {
       <div className="flex flex-col gap-[50px] py-[4.5rem] lg:px-[7rem] md:px-12 px-4 max-w-[87.5rem] justify-center items-center">
 
         {/* blogs */}
-        <div className="grid sm:grid-cols-2 gap-7 sm:gap-10 lg:gap-14">
-          <div className="flex flex-col order-2 w-full gap-2 text-left align-middle lg:gap-3">
-            <button className="text-[15px] capitalize sm:text-[18px] text-[#7A6C65] font-[inter] font-normal p-2 sm:p-[12px] border-[0.5px] max-w-[73px] rounded-[8px] ">
-              {blogs.length > 0 && blogs[0].blogType[0]}
-            </button>
-            <span className="font-[inter] font-normal text-[14px] sm:text-base w-[103px] h-[20px] text-[#7A6C65] ">
-              01 June 2023
-              {/* {blogs.length > 0 && new Date(blogs[0].date).getDate()} */}
+        {
+          data &&
+          <div className="grid sm:grid-cols-2 gap-7 sm:gap-10 lg:gap-14">
+            <div className="flex flex-col order-2 w-full gap-2 text-left align-middle lg:gap-3">
+              <button className="text-[15px] capitalize sm:text-[18px] text-[#7A6C65] font-[inter] font-normal p-2 sm:p-[12px] border-[0.5px] max-w-[73px] rounded-[8px] ">
+                {blogs.length > 0 && blogs[0].blogType[0]}
+              </button>
+              <span className="font-[inter] font-normal text-[14px] sm:text-base w-[103px] h-[20px] text-[#7A6C65] ">
+                01 June 2023
+                {/* {blogs.length > 0 && new Date(blogs[0].date).getDate()} */}
 
-            </span>
-            <b className="font-[inter] text-base sm:text-lg lg:text-2xl font-semibold text-[#101828] w-full ">
-              {/* {Object.keys(superBlog).length !== 0 ? superBlog.title : ' The Impact of DevOps on Software Development and Deployment'} */}
-              {blogs.length > 0 && blogs[0].title}
-            </b>
-            <p className="w-full text-sm font-normal lg:text-base">
-              {/* {Object.keys(superBlog).length !== 0 ? TextLength(superBlog.description || "") : 'How do you create compelling presentations that wow your audience...'} */}
-              {blogs.length > 0 && blogs[0].blogContent.slice(0, 100)}
-              {" ... "}
-              <a
-                className="underline text-text-dev-orange hover:opacity-70"
-                href="#"
-              >
-                Read more
-              </a>
-            </p>
-            <div className="flex items-center w-full gap-4">
-              <img
-                src="/Avatar.png"
-                className="w-10 h-10 rounded-[200px]"
-                alt="authorImg"
-              />
-              <span className="font-[inter] text-sm sm:text-base font-bold text-center ">
-                {blogs.length > 0 && blogs[0].author}
               </span>
+              <b className="font-[inter] text-base sm:text-lg lg:text-2xl font-semibold text-[#101828] w-full ">
+                {/* {Object.keys(superBlog).length !== 0 ? superBlog.title : ' The Impact of DevOps on Software Development and Deployment'} */}
+                {blogs.length > 0 && blogs[0].title}
+              </b>
+              <p className="w-full text-sm font-normal lg:text-base">
+                {/* {Object.keys(superBlog).length !== 0 ? TextLength(superBlog.description || "") : 'How do you create compelling presentations that wow your audience...'} */}
+                {blogs.length > 0 && blogs[0].blogContent.slice(0, 100)}
+                {" ... "}
+                <a
+                  className="underline text-text-dev-orange hover:opacity-70"
+                  href="#"
+                >
+                  Read more
+                </a>
+              </p>
+              <div className="flex items-center w-full gap-4">
+                <img
+                  src="/Avatar.png"
+                  className="w-10 h-10 rounded-[200px]"
+                  alt="authorImg"
+                />
+                <span className="font-[inter] text-sm sm:text-base font-bold text-center ">
+                  {blogs.length > 0 && blogs[0].author}
+                </span>
+              </div>
+            </div>
+            <div className="flex justify-end order-1 w-full h-full sm:order-2">
+              {blogs.length > 0 ?
+                <img
+                  className="w-356px h-155px object-cover sm:w-500px sm:h-full md:w-510px rounded-[8px]"
+                  src={blogs[0].blogImage}
+                  alt=""
+                />
+                :
+                <img
+                  className="w-356px h-155px sm:w-500px sm:h-full md:w-510px rounded-[8px]"
+                  src="/Image.png"
+                  alt=""
+                />
+              }
             </div>
           </div>
-          <div className="flex justify-end order-1 w-full h-full sm:order-2">
-            {blogs.length > 0 ?
-              <img
-                className="w-356px h-155px object-cover sm:w-500px sm:h-full md:w-510px rounded-[8px]"
-                src={blogs[0].blogImage}
-                alt=""
-              />
-              :
-              <img
-                className="w-356px h-155px sm:w-500px sm:h-full md:w-510px rounded-[8px]"
-                src="/Image.png"
-                alt=""
-              />
-            }
-          </div>
-        </div>
+        }
 
         <BlogsToDisplay blogs={blogs} currentPage={currentPage} />
 
