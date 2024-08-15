1cbb58a88a76d0819f3575af7f86edee8d03df14
adebabs-tec <ruqoyatomotayo@gmail.com>
2024-08-15 12:56:52 +0100

    changed no articles and no blogs content

--- client/src/components/regular/Blog/BlogContent/NoBlog.jsx
+++ client/src/components/regular/Blog/BlogContent/NoBlog.jsx
@@ -1,12 +1,13 @@
+import emptyfolder from "../../../../../public/empty-folder-svgrepo-com.svg";
 const NoBlog = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full text-center bg-gray-50 p-4">
+      <img src={emptyfolder} alt="" className="w-[5rem] h-[5rem] mb-2" />
       <p className="text-lg md:text-xl font-medium text-gray-700 mb-3">
         No blogs available at the moment.
       </p>
       <p className="text-base md:text-lg text-gray-500">
-        Explore other sections of our site while we prepare some amazing content
-        for you.
+        Fresh blogs coming soon!
       </p>
     </div>
   );
--- client/src/components/regular/Events/Context/NoCards.jsx
+++ client/src/components/regular/Events/Context/NoCards.jsx
@@ -1,11 +1,12 @@
+import { FaRegFile } from "react-icons/fa";
 const NoCards = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-full text-center p-6">
-      <p className="text-xl md:text-2xl font-semibold text-gray-800 mb-4">
-        Our events calendar is currently empty, but we’re planning some great
-        things for the future.
+    <div className="flex flex-col items-center justify-center h-full text-center py-2">
+      <FaRegFile className="text-6xl mb-2" />
+      <p className="text-xl md:text-2xl font-semibold text-gray-800 mb-2">
+        No Articles Yet
       </p>
-      <p className="text-lg md:text-xl text-gray-600">Stay connected!</p>
+      <p className="text-lg md:text-xl text-gray-600">coming soon...</p>
     </div>
   );
 };
